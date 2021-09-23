d06a5f3a4b3ef85d4af1f0aa1fb97e865f412f15
nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-09-23 23:21:41 +0600

    init: react native

---
+++ ios/Pods/Target Support Files/Pods-setHabits-setHabitsTests/Pods-setHabits-setHabitsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_setHabits_setHabitsTests : NSObject
+@end
+@implementation PodsDummy_Pods_setHabits_setHabitsTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-setHabits/Pods-setHabits-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_setHabits : NSObject
+@end
+@implementation PodsDummy_Pods_setHabits
+@end
